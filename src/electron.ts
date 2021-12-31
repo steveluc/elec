ac55238df0c6a8279204b17a9933731ddd839599
steveluc <steve@lucco.com>
2021-12-31 10:24:34 -0800

    add express server

--- src/electron.ts
+++ src/electron.ts
@@ -1,5 +1,18 @@
 import { app, BrowserWindow } from "electron";
+import * as express from "express";
+import { Request as EReq, Response as ERes } from "express";
 // import * as fs from "fs";
+
+const eapp = express();
+const port = 3008;
+
+eapp.get( "/", ( req: EReq, res: ERes ) => {
+  res.send( "Hello express world!" );
+} );
+
+eapp.listen( port, () => {
+  console.log( `server started at http://localhost:${ port }` );
+} );
 
 // fs.writeFileSync("testnode","Contents!");
 
@@ -13,4 +26,6 @@ function createWindow () {
       }
     });  // and load the index.html of the app.
     win.loadFile('../dist/index.html');
-  }app.on('ready', createWindow);
+  }
+  
+  app.on('ready', createWindow);
