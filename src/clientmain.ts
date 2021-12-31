d5b467863ebef4baf4c2d423f2ac3145e2774b9b
steveluc <steve@lucco.com>
2021-12-30 18:04:39 -0800

    webpack example start

---
+++ src/clientmain.ts
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+    const header = document.createElement("h1");
+    header.innerText = "Webpack❤️TS"
+  
+    const body = document.querySelector("body");
+    body.appendChild(header);
+    let div = document.createElement("div");
+    div.innerText = "Hello Henry!!!";
+    body.appendChild(div);
+  })
+  
